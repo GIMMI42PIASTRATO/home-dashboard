f82e30ebc7a258f5f7e38a786ecd8b161560f67d
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-08-05 23:25:27 +0100

    feat: add shooting stars background to the hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Navbar from "@/components/navbar";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
 
 export default function Home() {
 	return (
 		<main>
-			<div className="relative w-full flex items-center justify-center">
-				<Navbar className="top-2" />
+			<Navbar className="top-2" />
+			<div className="relative overflow-hidden">
+				<div className="max-w-7xl mx-auto px-4 flex min-h-screen flex-col items-center justify-between"></div>
+				<ShootingStars
+					minDelay={1000}
+					maxDelay={3000}
+					starColor="#45b2c8"
+				/>
+				<StarsBackground
+					minTwinkleSpeed={0.2}
+					maxTwinkleSpeed={0.5}
+					twinkleProbability={1}
+					starDensity={0.0005}
+				/>
 			</div>
 		</main>
 	);
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -7,8 +7,17 @@ export default function Page() {
 	return (
 		<div className="w-full h-screen flex justify-center items-center bg-neutral-950 relative ">
 			<SignIn />
-			<ShootingStars />
-			<StarsBackground minTwinkleSpeed={0.2} maxTwinkleSpeed={0.5} />
+			<ShootingStars
+				minDelay={1000}
+				maxDelay={3000}
+				starColor="#45b2c8"
+			/>
+			<StarsBackground
+				minTwinkleSpeed={0.2}
+				maxTwinkleSpeed={0.5}
+				twinkleProbability={1}
+				starDensity={0.0005}
+			/>
 		</div>
 	);
 }
