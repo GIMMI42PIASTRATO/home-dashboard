ad9ea9cb31b4f7f8b19df87bdf6171757cbdb361
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-08-08 16:02:34 +0200

    feat: add sing-in button and image in the hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,8 @@
 import AceternityButton from "@/components/ui/aceternity-button";
 import { Cover } from "@/components/ui/cover";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
 	return (
@@ -14,6 +17,22 @@ export default function Hero() {
 				Everything you need to manage your home smartly and efficiently,
 				making you feel like you are living in the 3000s.
 			</p>
+			<div className="flex items-center gap-4 justify-center mt-7 relative z-10">
+				<Button className="rounded-full" asChild>
+					<Link href="/sign-in">Get started</Link>
+				</Button>
+			</div>
+			<div className="p-4 border border-neutral-200 bg-neutral-100 dark:bg-neutral-800 dark:border-neutral-700 rounded-[32px] mt-20 relative">
+				<div className="absolute inset-x-0 bottom-0 h-40 w-full bg-gradient-to-b from-transparent via-white to-white dark:via-black/50 dark:to-black scale-[1.1] pointer-events-none"></div>
+				<div className="p-2 bg-white dark:bg-black dark:border-neutral-700 border border-neutral-200 rounded-[24px]">
+					<Image
+						src="/macOS_Dark.png"
+						width={1920}
+						height={1080}
+						alt="dashboard preview"
+					></Image>
+				</div>
+			</div>
 		</section>
 	);
 }
