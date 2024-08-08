5673a725aa2d9959a6c2746ea88cc002f85862d6
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-08-08 14:46:15 +0200

    feat: Update hero component with futuristic dashboard content

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,6 +6,13 @@ export default function Hero() {
 			<div className="flex justify-center">
 				<AceternityButton />
 			</div>
+			<h1 className="text-2xl md:text-4xl lg:text-8xl font-semibold max-w-6xl mx-auto text-center mt-6 relative z-10">
+				The futiristic dashboard for your home
+			</h1>
+			<p className="text-center mt-8 text-base md:text-xl text-neutral-600 dark:text-neutral-300 max-w-3xl mx-auto relative z-10">
+				Everything you need to manage your home smartly and efficiently,
+				making you feel like you are living in the 3000s.
+			</p>
 		</section>
 	);
 }
